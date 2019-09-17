46564419bdf4542268c4ccb04e6e848814020dfa
FressiaWang <fressiawang@163.com>
2019-09-17 16:35:08 -0400

    Create ZigZag Conversion.py

---
+++ Interview145/ZigZag Conversion.py
@@ -0,0 +1,40 @@
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+"""
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1: return s
+        period = numRows + numRows -2
+        d = {i: i if i < numRows else (period -i) for i in range(period)}
+        out = ["" for i in range(numRows)]
+        for i in range(len(s)):
+            out[d[i%period]] += s[i]
+        return "".join(out)
