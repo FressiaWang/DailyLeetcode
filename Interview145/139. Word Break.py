50206d84b44e869a97332b41a0ecd34b76421df4
FressiaWang <fressiawang@163.com>
2019-10-17 17:14:34 -0400

    Create 139. Word Break.py

---
+++ Interview145/139. Word Break.py
@@ -0,0 +1,43 @@
+"""
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+
+DP
+"""
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        n = len(s)
+        res = [False] * (n+1)
+        res[0] = True
+        for i in range(n+1):
+            if res[i]:
+                for j in wordDict:
+                    l = len(j)
+                    if (i+l) <= n and s[i:i+l] == j:
+                        res[i+l]=True
+        return res[n]
