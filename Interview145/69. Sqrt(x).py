2c0e783ed7a5c89b61c5fda28cd863923ad698d0
FressiaWang <fressiawang@163.com>
2020-01-31 16:49:09 -0500

    Update 69. Sqrt(x).py

--- Interview145/69. Sqrt(x).py
+++ Interview145/69. Sqrt(x).py
@@ -46,3 +46,19 @@ def mySqrt(self, x):
                 h = mid 
             else:
                 l = mid + 1
+def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        #if x == x*x: return x
+        l, h = 0, x
+        while l < h:
+            mid = (l + h) // 2
+            if mid * mid <= x < (mid + 1) * (mid + 1):
+                return mid
+            elif mid * mid > x:
+                h = mid 
+            else:
+                l = mid + 1
+        return l
