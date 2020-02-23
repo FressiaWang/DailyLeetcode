32600624a5f272de495339f594850fba03ba5aa5
FressiaWang <fressiawang@163.com>
2020-02-23 15:26:21 -0500

    Create 189. Rotate Array.py

---
+++ Interview145/189. Rotate Array.py
@@ -0,0 +1,51 @@
+"""
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: [-1,-100,3,99] and k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+Note:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+
+https://leetcode.com/problems/rotate-array/discuss/269948/4-solutions-in-python-(From-easy-to-hard)
+"""
+class Solution3:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        count = 0
+        start = 0
+        while count < len(nums):
+            current = start 
+            prev = nums[start] #store the value in the position
+            
+            while True:
+                next = (current + k) % len(nums) #
+                temp = nums[next] #store it temporaly 
+                nums[next] = prev #overide the next 
+                prev = temp #reset prev
+                current = next #reset current
+                count += 1
+
+                if start == current:
+                    break 
+            
+            start += 1
