21a1be54b680fa098e5d8749eef849bd223c5b54
FressiaWang <fressiawang@163.com>
2020-03-11 10:26:27 -0400

    Create 74. Search a 2D Matrix.py

---
+++ Interview145/74. Search a 2D Matrix.py
@@ -0,0 +1,58 @@
+"""
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+"""
+def searchMatrix(self, matrix, target):
+        if not matrix or target is None:
+            return False
+
+        rows, cols = len(matrix), len(matrix[0])
+        low, high = 0, rows * cols - 1
+        
+        while low <= high:
+            mid = (low + high) / 2
+            num = matrix[mid / cols][mid % cols]
+
+            if num == target:
+                return True
+            elif num < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return False
+        
+def searchMatrix(matrix, target):
+    if not matrix or not matrix[0]: return False 
+    rows, cols = len(matrix), len(matrix[0])
+    l, r = 0, rows *  cols - 1
+    while l < r:
+        mid = (l + r) // 2
+        ## position of mid map to 2d matrix
+        if matrix[mid // col][mid % col] < target:
+            l = mid + 1
+        else:
+            r = mid 
+    return target == matrix[l//col][l % col]        
