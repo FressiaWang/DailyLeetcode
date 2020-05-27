1977f97ba34e019c77464ac2812d58b3fc7d5601
FressiaWang <fressiawang@163.com>
2020-05-27 15:33:04 -0400

    Create 232. Implement Queue using Stacks.py

---
+++ Interview145/232. Implement Queue using Stacks.py
@@ -0,0 +1,31 @@
+"""
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+"""
+
+#Approach 2: push O(1), pop amortized O(1)
+
+class MyQueue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x):
+        self.s1.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self):
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]        
+
+    def empty(self):
+        return not self.s1 and not self.s2
