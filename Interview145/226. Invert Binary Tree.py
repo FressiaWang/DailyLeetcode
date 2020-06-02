6f9ae69c259e20f4810e4163f73aa8098a8e99ae
FressiaWang <fressiawang@163.com>
2020-06-02 17:01:33 -0400

    Create 226. Invert Binary Tree.py

---
+++ Interview145/226. Invert Binary Tree.py
@@ -0,0 +1,42 @@
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root:
+            root.left, root.right = root.right, root.left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+        return root
+ def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        stack = root and [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack += node.left, node.right
+        return root       
