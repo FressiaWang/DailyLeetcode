deecc6fd09f874ae886679cddd923e8e7febf24e
FressiaWang <fressiawang@163.com>
2020-06-02 10:22:41 -0400

    Update and rename Merge Two Sorted Lists.py to 21.Merge Two Sorted Lists.py

--- Interview145/21.Merge Two Sorted Lists.py
+++ Interview145/21.Merge Two Sorted Lists.py
@@ -48,3 +48,14 @@ class Solution(object):
                 l1,l2 = None,None
 
         return head.next
+
+    def mergeTwoLists(l1, l2):
+    # 判断是否存在None
+    if not l1 or not l2:
+        return l1 or l2
+    if l1.val < l2.val:
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
