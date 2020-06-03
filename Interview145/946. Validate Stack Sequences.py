e5751f1f5c771217c93dbee225ba9a4b8b0e68b1
FressiaWang <fressiawang@163.com>
2020-06-03 13:45:58 -0400

    Create 946. Validate Stack Sequences.py

---
+++ Interview145/946. Validate Stack Sequences.py
@@ -0,0 +1,29 @@
+"""
+Given two sequences pushed and popped with distinct values, 
+return true if and only if this could have been the result 
+of a sequence of push and pop operations on an initially empty stack.
+
+ 
+
+Example 1:
+
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+Output: true
+Explanation: We might do the following sequence:
+push(1), push(2), push(3), push(4), pop() -> 4,
+push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+"""
+def validateStackSequences(self, pushed, popped):
+        """
+        :type pushed: List[int]
+        :type popped: List[int]
+        :rtype: bool
+        """
+        j = 0
+        stack = []
+        for x in pushed:
+            stack.append(x)
+            while stack and j < len(popped) and stack[-1] == popped[j]:
+                stack.pop()
+                j += 1
+        return j == len(popped)
